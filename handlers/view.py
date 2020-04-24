05f4a04729f3b4dd5759a6de35311dfc62ab728a
Maharaj Fawwaz Almuqaddim Yusran <fawwazyusran@gmail.com>
2020-04-24 22:30:01 +0800

    Use bash script with timeout instead to fix bot server crashing

--- handlers/view.py
+++ handlers/view.py
@@ -1,9 +1,15 @@
+from telegram import Bot, ParseMode
+
 from database.database import get_participants, get_latest_setoran
 
 
-def display_setoran(bot, chat_id):
+def display_setoran(bot: Bot, chat_id):
     participants = get_participants()
-    text = ""
+
+    final_text = """o  ⁠⁠⁠بِسْـــــــــمِ اللهِ الرَّحْمَٰنِ الرَّحِيْمِ    o
+    *LAPORAN KHATAM QUR'AN RAMADHAN 1441H*
+    
+    """
     for i in range(len(participants)):
         id = participants[i][0]
         full_name = participants[i][1].capitalize()
@@ -11,11 +17,11 @@ def display_setoran(bot, chat_id):
 
         juz_no, juz_part = get_latest_setoran(id)
 
-        text += f"""{i + 1}. {full_name} """
+        final_text += f"""{i + 1}. {full_name} """
 
         if juz_no > 0:
-            text += f"""Juz {juz_no} {juz_part.upper()}"""
+            final_text += f"""Juz {juz_no} {juz_part.upper()}"""
 
-        text += f"""\nTotal kholas: {completed_half_juz}\n\n"""
+        final_text += f"""\nTotal kholas: {completed_half_juz}\n\n"""
 
-    bot.send_message(chat_id=chat_id, text=text)
+    bot.send_message(chat_id=chat_id, text=final_text, parse_mode=ParseMode.MARKDOWN_V2)
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+while true; do
+#  echo "Restarting bot server..."
+  timeout 5 python main.py
+done
